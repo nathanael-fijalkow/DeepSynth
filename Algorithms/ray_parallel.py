a9b624e44423f12b937e36c5029529f269a98253
Theomat <theomatricon@gmail.com>
2021-09-02 19:02:05 +0200

    Little improvements

--- Algorithms/ray_parallel.py
+++ Algorithms/ray_parallel.py
@@ -10,7 +10,6 @@ import time
 #   File "[..]/ray/actor.py", line 823, in __del__
 # AttributeError: 'NoneType' object has no attribute 'global_worker'
 # You need to apply this patch: https://github.com/ray-project/ray/pull/16955/commits/faa8d6b200f52d86fa5ace2506739d88a767c03b
-ray.init(ignore_reinit_error=True)
 
 @ray.remote
 class Producer:
@@ -59,7 +58,7 @@ class Filter:
                         self.to_queue.put(item)
             except Empty:
                 times_without_new_data += 1
-                time.sleep(self.sleep_delay)     
+                time.sleep(self.sleep_delay)  
         return n
 
 
@@ -70,7 +69,7 @@ def make_parallel_pipelines(create_generators, make_filter_fn, nb_filters,
     Parameters:
     -----------
     - create_generators: list[unit -> Generator['a]]  - functions that instanciate the generators used by producers, 1 generator = 1 producer
-    - make_filter_fn: unit -> 'a -> bool              - create the filter function f: 'a -> bool, if f(a) the filter add this item to the output queue otherwise discards it 
+    - make_filter_fn: int -> 'a -> bool              - create the filter function f: 'a -> bool, if f(a) the filter add this item to the output queue otherwise discards it 
     - nb_filters: int                                 - the number of filters actors to make
     - generation_queue_size: int                      - the max_size of the queue used to communicate between the producers and the filters
     - output_queue_size: int                          - the max_size of the queue the filters output to
@@ -90,8 +89,9 @@ def make_parallel_pipelines(create_generators, make_filter_fn, nb_filters,
     
     producers = [Producer.remote(
         cr, from_queue, batch_size) for cr in create_generators]
-    filters = [Filter.remote(ray.put(make_filter_fn()), from_queue, to_queue, batch_size, filters_check_delay, filters_stop_number)
-               for _ in range(nb_filters)]
+    filters_fn_ref = [make_filter_fn(i) for i in range(nb_filters)]
+    filters = [Filter.remote(filters_fn_ref[i], from_queue, to_queue, batch_size, filters_check_delay, filters_stop_number)
+               for i in range(nb_filters)]
 
     return producers, filters, from_queue, to_queue
         
@@ -107,7 +107,7 @@ def start(producers):
 if __name__ == "__main__":
     def make_generator(k):
         return lambda: (x for x in range(k))
-    make_consume_fn = lambda: lambda x: x == 7
+    make_consume_fn = lambda _: lambda x: x == 7
     create_generators = [make_generator(k) for k in range(10)]
     producers, filters, from_queue, to_queue = make_parallel_pipelines(
         create_generators, make_consume_fn, 2, 100, 100, 1)
