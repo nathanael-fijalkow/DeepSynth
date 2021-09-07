a3421cb96aad75e828fdfe571ae958371c7d17f5
Theomat <theomatricon@gmail.com>
2021-09-07 16:36:06 +0200

    Improved list DSL for dreamcoder experiments

--- DSL/list.py
+++ DSL/list.py
@@ -89,7 +89,7 @@ def _a1(x): return x + 1
 def _d1(x): return x - 1
 
 
-def _mod(x): return lambda y: x % y if y != 0 else None
+def _mod(x): return lambda y: y % x if x != 0 else None
 
 
 def _not(x): return not x
@@ -216,13 +216,22 @@ def _fix2(a1):
 def _match(l):
     return lambda b: lambda f: b if l == [] else f(l[0])(l[1:])
 
+
+def _miter(k, f, x):
+    if k <= 0:
+        return x
+    return _miter(k-1, f, f(x))
 semantics = {
         "empty" : [],
         "cons" : _cons,
         "car" : _car,
         "cdr" : _cdr,
         "empty?" : _isEmpty,
-        "gt?" : _gt,
+        "gt?": _gt,
+        "le?": lambda x: lambda y: x <= y,
+        "not": lambda x: not x,
+        "max": lambda x: lambda y: max(x, y),
+        "min": lambda x: lambda y: min(x, y),
         "if" : _if,
         "eq?" : _eq,
         "*" : _multiplication,
@@ -235,14 +244,18 @@ semantics = {
         "3" : 3,
         "4" : 4,
         "5" : 5,
+        "append": lambda x: lambda l: l + [x],
         "range" : _range,
         "map" : _map,
         "unfold" : _unfold,
         "index" : _index,
         "fold" : _fold,
-        "mod" : _mod,
+        "is-mod": lambda x: lambda y: y % x == 0 if x != 0 else False,
+        "mod": _mod,
+        "iter": _miter,
         "is-prime" : _isPrime,
         "is-square" : _isSquare,
+        "filter": lambda f: lambda l: [x for x in l if f(x)]
         }
 
 primitive_types = {
@@ -251,7 +264,11 @@ primitive_types = {
         "car": Arrow(List(t0), t0),
         "cdr": Arrow(List(t0), List(t0)),
         "empty?": Arrow(List(t0), BOOL),
+        "max": Arrow(INT, Arrow(INT, INT)),
+        "min": Arrow(INT, Arrow(INT, INT)),
         "gt?": Arrow(INT, Arrow(INT, BOOL)),
+        "le?": Arrow(INT, Arrow(INT, BOOL)),
+        "not": Arrow(BOOL, BOOL),
         "if": Arrow(BOOL, Arrow(t0, Arrow(t0, t0))),
         "eq?": Arrow(INT, Arrow(INT, BOOL)),
         "*": Arrow(INT, Arrow(INT, INT)),
@@ -266,10 +283,18 @@ primitive_types = {
         "5": INT,
         "range": Arrow(INT, List(INT)),
         "map": Arrow(Arrow(t0, t1), Arrow(List(t0), List(t1))),
+        "iter": Arrow(INT, Arrow(Arrow(t0, t0), Arrow(t0, t0))),
+        "append": Arrow(t0, Arrow(List(t0), List(t0))),
         "unfold": Arrow(t0, Arrow(Arrow(t0,BOOL), Arrow(Arrow(t0,t1), Arrow(Arrow(t0,t0), List(t1))))),
         "index": Arrow(INT, Arrow(List(t0), t0)),
         "fold": Arrow(List(t0), Arrow(t1, Arrow(Arrow(t0, Arrow(t1, t1)), t1))),
+        "is-mod": Arrow(INT, Arrow(INT, BOOL)),
         "mod": Arrow(INT, Arrow(INT, INT)),
         "is-prime": Arrow(INT, BOOL),
         "is-square": Arrow(INT, BOOL),
+        "filter": Arrow(Arrow(t0, BOOL), Arrow(List(t0), List(t0))),
         }
+
+# (unfold (* (mod 5 0) (index 4 var0)) false)
+# fold: 'a list -> ('b -> 'a -> 'b) -> 'b -> 'b
+# (range (fold (cdr var0) -2 pow))
