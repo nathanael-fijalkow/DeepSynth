ad012d7e90b3b6e1f9a36cd0a5140344c474087e
Theomat <theomatricon@gmail.com>
2021-09-07 15:26:11 +0200

    Improved experiment

--- run_randomPCFG_search_parallel.py
+++ run_randomPCFG_search_parallel.py
@@ -1,3 +1,4 @@
+import torch
 from ray.util.queue import Empty
 from Algorithms.ray_parallel import make_parallel_pipelines, start
 from cons_list import cons_list2list
@@ -35,7 +36,7 @@ from DSL.deepcoder import semantics, primitive_types
 logging_levels = {0: logging.INFO, 1: logging.DEBUG}
 verbosity = 0
 logging.basicConfig(format='%(message)s', level=logging_levels[verbosity])
-
+split_numbers = [2, 6]
 seed = 100
 random.seed(seed)
 np.random.seed(seed)
@@ -99,8 +100,6 @@ def reconstruct_from_list(program_as_list, target_type):
 
 def insert_prefix_toprog(prefix, prog, target_type):
     prefix = cons_list2list(prefix)
-    # print("Reconstructing:", prefix, "(", type(
-    # prefix[0]), ")", "prog:", prog, "(", type(prog), ")")
     return reconstruct_from_list([prog] + prefix, target_type)
 
 
@@ -124,11 +123,11 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
 
         def add_search_data(self, i, t, probability) -> bool:
             self.search_times[i] += t
-            self.prob += probability
-            self.probabilities.append(self.prob)
             mini = min(self.search_times)
             self.times.append(mini)
+            self.prob += probability
             self.programs += 1
+            # self.probabilities.append(self.prob)
             if mini > timeout or self.programs > max_number_programs:
                 # print("Should stop because: time=", self.total_time,
                 #       "programs=", self.programs, "prob=", self.prob)
@@ -148,8 +147,7 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
                 p = next(gen)
                 probability = pcfg.probability_program(
                     pcfg.start, reconstruct_from_compressed(
-                        p, target_type)
-                )
+                        p, target_type))
                 data_collector.add_search_data.remote(i, 0, probability)
                 try:
                     while True:
@@ -158,9 +156,8 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
                         prog = insert_prefix(prefix, p)
                         t += time.perf_counter()
                         prog_r = reconstruct_from_compressed(prog, target_type)
-                        probability = pcfg.probability_program(
-                            pcfg.start, prog_r)
-                        if ray.get(data_collector.add_search_data.remote(i, t, probability)):
+                        # probability = pcfg.probability_program(pcfg.start, prog_r)
+                        if ray.get(data_collector.add_search_data.remote(i, t, 0)):
                             break
                         # yield prog
                 except StopIteration:
@@ -171,10 +168,8 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
                 gen = algorithm(cur_pcfg)
                 target_type = pcfg.start[0]
                 p = next(gen)
-                probability = pcfg.probability_program(
-                    pcfg.start, p if prefix is None else insert_prefix_toprog(
-                        prefix, p, target_type)
-                )
+                probability = pcfg.probability_program(pcfg.start, p if prefix is None else insert_prefix_toprog(
+                    prefix, p, target_type))
                 data_collector.add_search_data.remote(i, 0, probability)
                 try:
                     while True:
@@ -191,12 +186,10 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
                             prog = insert_prefix_toprog(
                                 prefix, p, target_type)
                             t += time.perf_counter()
+                        # probability = pcfg.probability_program(
+                        #     pcfg.start, prog)
 
-                        # print(f"Generated:", prog)
-                        # break
-                        probability = pcfg.probability_program(
-                            pcfg.start, prog)
-                        if ray.get(data_collector.add_search_data.remote(i, t, probability)):
+                        if ray.get(data_collector.add_search_data.remote(i, t, 0)):
                             break
                         # yield prog
                 except StopIteration:
@@ -249,7 +242,7 @@ def run_algorithm_parallel(pcfg: PCFG, algo_index: int, splits: int,
 
     logging.info("\tShut down.")
 
-    return [[search_times[i], cumulative_probabilities[i]] for i in range(len(search_times))]
+    return search_times
 
 
 def create_dataset():
@@ -258,76 +251,62 @@ def create_dataset():
 
     timepoints = np.logspace(
         start=-3, stop=log10(timeout), num=number_timepoints)
-    countpoints = np.linspace(
-        start=0, stop=max_number_programs, num=number_countpoints)
+    r_program = np.zeros(
+        (number_samples, len(split_numbers) * number_algorithms, number_countpoints))
+    for i in range(number_samples):
+        deepcoder_PCFG = deepcoder_CFG.CFG_to_Random_PCFG()
 
-    for splits in [2, 6]:
-        r_time = np.zeros(
-            (number_samples, number_algorithms, number_timepoints))
-        r_program = np.zeros(
-            (number_samples, number_algorithms, number_countpoints))
-        for i in range(number_samples):
-            deepcoder_PCFG = deepcoder_CFG.CFG_to_Random_PCFG()
-
+        for j, splits in enumerate(split_numbers):
             for algo_index in range(number_algorithms):
                 algorithm, name_algo, param = list_algorithms[algo_index]
 
-                logging.info('start run number {}: {}'.format(i+1, name_algo))
+                logging.info('start run number {}: {} {}'.format(
+                    i+1, name_algo, splits))
 
                 res = run_algorithm_parallel(
                     pcfg=deepcoder_PCFG, algo_index=algo_index, splits=splits, n_filters=1)
-                r_time[i][algo_index] = np.interp(timepoints,
-                                                  [search_time for search_time,
-                                                   _ in res],
-                                                  [cumulative_probability for _, cumulative_probability in res])
-                r_program[i][algo_index] = np.interp(countpoints,
-                                                     range(len(res)),
-                                                     [cumulative_probability for _, cumulative_probability in res])
+                # r_time[i][algo_index] = np.interp(timepoints,
+                #                                   [search_time for search_time,
+                #                                    _ in res],
+                #                                   [cumulative_probability for _, cumulative_probability in res])
+                r_program[i][algo_index * number_algorithms + j] = np.interp(timepoints,
+                                                                             res,
+                                                                            range(len(res)))
 
                 logging.info('finished run number {}'.format(i+1))
 
-        result_time_mean = np.mean(r_time, axis=0)
-        result_time_std = np.std(r_time, axis=0)
+        # result_time_mean = np.mean(r_time, axis=0)
+        # result_time_std = np.std(r_time, axis=0)
 
-        result_program_mean = np.mean(r_program, axis=0)
-        result_program_std = np.std(r_program, axis=0)
+    result_program_mean = np.mean(r_program, axis=0)
+    result_program_std = np.std(r_program, axis=0)
 
-        for j, algo_index in enumerate(range(number_algorithms)):
-            algorithm, name_algo, param = list_algorithms[algo_index]
-
-            with open('results_syntactic/cumulative_probability_vs_time_{}_{}_splits{}.csv'.format(name_algo, timeout, splits), 'w', encoding='UTF8', newline='') as f:
+    for algo_index in range(number_algorithms):
+        algorithm, name_algo, param = list_algorithms[algo_index]
+        for j, splits in enumerate(split_numbers):
+            index = algo_index * number_algorithms + j
+            with open('results_syntactic/time_vs_number_programs_{}_{}_splits{}.csv'.format(name_algo, timeout, splits), 'w', encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
-                header = ['search time', 'mean cumulative probability',
-                          'standard deviation']
+                header = ['time', 'mean number of programs'
+                          , 'standard deviation']
                 writer.writerow(header)
                 for x, t in enumerate(timepoints):
                     writer.writerow(
-                        (t, result_time_mean[j][x], result_time_std[j][x]))
-
-            with open('results_syntactic/cumulative_probability_vs_number_programs_{}_{}_splits{}.csv'.format(name_algo, timeout, splits), 'w', encoding='UTF8', newline='') as f:
-                writer = csv.writer(f)
-                header = ['number of programs',
-                          'mean cumulative probability', 'standard deviation']
-                writer.writerow(header)
-                for x in range(number_countpoints):
-                    writer.writerow(
-                        (x, result_program_mean[j][x], result_program_std[j][x]))
-
-# Plot cumulative probability VS time
+                        (t, result_program_mean[index][x], result_program_std[index][x]))
 
 
-def plot_cumulative_probability_vs_time():
-    logging.info('Plot cumulative probability VS time')
-    for splits in [4, 10]:
+def plot_programs_vs_time():
+    logging.info('Plot programs VS time')
+    for splits in split_numbers:
         for algo_index in range(len(list_algorithms)):
             algorithm, name_algo, param = list_algorithms[algo_index]
             # timepoints = np.arange(start = 0, stop = number_timepoints)
             timepoints = np.logspace(
                 start=-3, stop=log10(timeout), num=number_timepoints)
 
-            logging.info('retrieve run: {}'.format(name_algo))
+            logging.info('retrieve run: {} {}'.format(name_algo, splits))
 
-            with open('results_syntactic/cumulative_probability_vs_time_{}_{}.csv'.format(name_algo, timeout), 'r', encoding='UTF8', newline='') as f:
+            with open('results_syntactic/time_vs_number_programs_{}_{}_splits{}.csv'.format(name_algo, timeout, splits), 'r', encoding='UTF8', newline='') as f:
                 reader = csv.reader(f)
                 result_mean = np.zeros(number_timepoints)
                 result_std = np.zeros(number_timepoints)
@@ -351,56 +330,10 @@ def plot_cumulative_probability_vs_time():
     plt.xlim((1e-3, timeout))
     plt.xlabel('time (in seconds)')
     plt.xscale('log')
-    plt.ylim((0, 1))
-    plt.ylabel('cumulative probability')
+    # plt.ylim((0, max_number_programs))
+    plt.ylabel('number of programs')
 
-    plt.savefig("results_syntactic/cumulative_probability_vs_time_%s.png" % seed,
-                dpi=500,
-                bbox_inches='tight')
-    plt.clf()
-
-
-# Plot cumulative probability VS number of programs
-def plot_cumulative_probability_vs_number_programs():
-    logging.info('Plot cumulative probability VS number of programs')
-    countpoints = np.linspace(
-        start=0, stop=max_number_programs, num=number_countpoints)
-
-    for algo_index in range(len(list_algorithms)):
-        algorithm, name_algo, param = list_algorithms[algo_index]
-        # heap search and A* are the same here
-        if name_algo == 'A*':
-            continue
-
-        logging.info('retrieve run: {}'.format(name_algo))
-
-        with open('results_syntactic/cumulative_probability_vs_number_programs_{}_{}.csv'.format(name_algo, timeout), 'r', encoding='UTF8', newline='') as f:
-            reader = csv.reader(f)
-            result_mean = np.zeros(number_countpoints)
-            result_std = np.zeros(number_countpoints)
-            for i, row in enumerate(reader):
-                if i == 0:
-                    continue
-                result_mean[i-1] = row[1]
-                result_std[i-1] = row[2]
-
-        logging.info('retrieved')
-
-        result_top = result_mean + .5 * result_std
-        result_low = result_mean - .5 * result_std
-        sc = plt.scatter(countpoints, result_mean, label=name_algo, s=5)
-        color = sc.get_facecolors()[0].tolist()
-        plt.fill_between(countpoints, result_top, result_low,
-                         facecolor=color, alpha=0.2)
-
-    plt.ticklabel_format(axis='x', style='sci', scilimits=(3, 5))
-    plt.xlabel('number of programs')
-    plt.xlim((0, max_number_programs))
-    plt.ylabel('cumulative probability')
-    plt.ylim((0, 1))
-    plt.legend(loc='lower right')
-
-    plt.savefig("results_syntactic/cumulative_probability_vs_number_programs_%s.png" % seed,
+    plt.savefig("results_syntactic/programs_vs_time_%s_parallel.png" % seed,
                 dpi=500,
                 bbox_inches='tight')
     plt.clf()
@@ -409,11 +342,10 @@ def plot_cumulative_probability_vs_number_programs():
 number_samples = 5
 
 number_timepoints = 1_000
-timeout = 2
+timeout = 1
 
 number_countpoints = 1_000
-max_number_programs = 5e5
+max_number_programs = 2e6
 ray.init()
 create_dataset()
-plot_cumulative_probability_vs_time()
-plot_cumulative_probability_vs_number_programs()
+plot_programs_vs_time()
